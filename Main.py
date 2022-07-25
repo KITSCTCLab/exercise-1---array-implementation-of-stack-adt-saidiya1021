b9f8b6de2b2c9ff2845744969a57d306ac6fb1f8
saidiya1021 <109952168+saidiya1021@users.noreply.github.com>
2022-07-25 15:54:50 +0530

    final commit

--- Main.py
+++ Main.py
@@ -1,24 +1,38 @@
 import os
 class Stack:
+    """This class contains the stack and its size
+    Attributes:
+        items: it is the stack
+        size: it holds the size of the stack"""
+    
     def __init__(self, size):
+      
         self.items = []
         self.size = size
 
     def is_empty(self):
+        """The is_empty() function returns if the stack is empty"""
         return len(self.items)==0
 
     def is_full(self):
+        """The is_full() function returns if the stack is full"""
         return len(self.items)==self.size
 
     def push(self, data):
+        """The push function pushes or adds the data to the stack
+        Arguments: 
+        data: it holds the value to be added to the stack"""
+        
         if not self.is_full():
             self.items.append(data)
 
     def pop(self):
+        """The pop function pops or deletes the top most data from the stack"""
         if not self.is_empty():
             self.items.pop(-1)
 
     def status(self):
+        """The status function displays the elements of the stack"""
         for element in self.items:
             print(element)
 
